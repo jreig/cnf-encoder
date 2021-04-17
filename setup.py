180611ce31d66303571302fcd0d0e58e5696318d
Joan Reig <joan.reig27@gmail.com>
2021-04-17 20:03:10 +0200

    Improve project organization

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 import setuptools
 
 setuptools.setup(
-  name="cnf-encoder",
-  version="0.0.1a",
-  author="Joan Reig",
-  author_email="joan.reig27@gmail.com",
-  keywords="cnf encoder sat tseytin tseitin",
-  description="CNF encoder of propositional formulas using Tseytin transformation.",
-  long_description=open("README.md").read(),
-  long_description_content_type="text/markdown",
-  packages=setuptools.find_packages(),
-  license="MIT",
-  url="https://github.com/jreig/cnf-encoder",
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-  ],
+    author_email="joan.reig27@gmail.com",
+    author="Joan Reig",
+    classifiers=["Intended Audience :: Developers"],
+    description="CNF encoder of propositional formulas using Tseytin transformation.",
+    keywords="cnf encoder sat tseytin tseitin",
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    name="cnfencoder",
+    packages=setuptools.find_packages(),
+    test_require=['nose'],
+    test_suit='nose.collector',
+    url="https://github.com/jreig/cnf-encoder",
+    version="0.1"
 )
